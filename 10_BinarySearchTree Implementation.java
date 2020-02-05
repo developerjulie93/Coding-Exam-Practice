f1431e0565c3ffac1709f19339e859ab5f062b2f
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-02-04 23:50:32 -0800

    Update 10_BinarySearchTree Implementation.java

--- 10_BinarySearchTree Implementation.java
+++ 10_BinarySearchTree Implementation.java
@@ -17,9 +17,8 @@ public class BinarySearchTree{
         root = insertProps(root, input);
     }
     Node insertProps(Node root, int input){
-        if(size == 0){
+        if(root == null){
             root = new Node(input);
-            size++;
             return root;
         }
         if(input < root.data){
@@ -30,8 +29,26 @@ public class BinarySearchTree{
         }
         return root;
     }
+    void inorder(){
+        inorderProps(root);
+    }
+    void inorderProps(Node root){
+        if(root !=null){
+            inorderProps(root.left);
+            System.out.println(root.data);
+            inorderProps(root.right);
+        }
+    }
     public static void main(String[] args){
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(7);
+        tree.insert(4);
+        tree.insert(1);
+        tree.insert(8);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(3);
+        tree.insert(5);
+        tree.inorder();
     }
 }
