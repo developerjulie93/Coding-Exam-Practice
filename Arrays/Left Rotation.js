1edeb30a761546ba7f4ef2df499045745ff6e4c4
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-03-06 12:03:35 -0800

    Create Left Rotation.js
    배열의 크기가 클 경우 timeout  이 됨

---
+++ Arrays/Left Rotation.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+
+
+}
+
+function main() {
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+    let ar = a;
+
+    let arr = [];
+    for(let i=0; i<d; i++){
+        arr = ar.slice(1,ar.length);
+        arr.push(ar[0]);
+        ar = arr;
+    }
+   
+       console.log(arr.join(' '));
+    
+}
