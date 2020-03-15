a88c9726b43bf283f2c6b73885f07e092211d0c9
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-03-14 23:44:48 -0700

    Update Sock Merchant.js

--- Js/Sock Merchant.js
+++ Js/Sock Merchant.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
@@ -13,36 +11,22 @@ process.stdin.on('data', inputStdin => {
 });
 
 process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
 });
 
 function readLine() {
     return inputString[currentLine++];
 }
 
-// Complete the sockMerchant function below.
-
-function main() {
-    const n = parseInt(readLine(), 10);
-    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
-
-    let pair = 0;
-    let unpaired = [];
-
-    for(let elmn of ar){
-        if(unpaired[elmn]){
-            unpaired[elmn]++;
-            if(unpaired[elmn] % 2 ==0){
-                pair++;
-            }
-        }
-        else{
-            unpaired[elmn] = 1;
-        }
-    }
-    console.log(pair);
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    
 }
+
